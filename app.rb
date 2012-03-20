deaf7f6e41fd14ba31e47cbf9ab68a3b5b0a1526
Richard Morrison <richard.morrison@owlstone.co.uk>
2012-03-20 18:09:53 +0000

    Load config from a file, allow multiple text widgets with multiple pages

--- app.rb
+++ app.rb
@@ -3,35 +3,46 @@ require 'sinatra'
 require 'json'
 require 'geckoboard-push'
 
-enable :sessions
+class Hash
+  def symbolize_keys!
+    keys.each do |key|
+      self[(key.to_sym rescue key) || key] = delete(key)
+    end
+    self
+  end
+end
 
 get '/' do
+  @config = YAML.load_file("config.yml")
+  @widgets = @config['widgets']
 
-  if session[:data].nil? then
-    session[:data] = {
-      # if set, environment variable GECKOBOARD_API_KEY will be used and the user won't be asked for API key
-      'api_key' => ENV['GECKOBOARD_API_KEY']  
-    }
-  end
+  # if set, environment variable GECKOBOARD_API_KEY will override settings.yml
+  @api_key = ENV['GECKOBOARD_API_KEY'] || @config['geckoboard_api_key']
   
   erb :index
 end
 
 post '/push' do
+  params.symbolize_keys!
 
-  # take the three parameters and push to Geckoboard
+  # take the parameters and push to Geckoboard
   begin
-    Geckoboard::Push.api_key = params['api_key']
-    response = Geckoboard::Push.new(params['widget_key']).text [{:text => params['text']}]
-    session[:message] = "Successfully updated widget!"
+    Geckoboard::Push.api_key = params[:api_key]
+
+    if params[:widget_type] == "text"
+      # symbolize keys and only send if there's actually a value
+      data = params[:widget_data].map(&:symbolize_keys!).select{|t| !t[:text].empty? }
+      raise 'No input data' if data.empty?
+      it_worked = Geckoboard::Push.new(params[:widget_key]).text(data)
+    else
+      raise "Not supporting '#{params[:widget_type]}' widgets yet."
+    end
+    msg = (it_worked == true ? "Successfully updated widget!" : "Something went wrong, sorry.")
   rescue Exception => e
     # oops, got an error. Try to display to the user
-    # puts e
     puts e.message
-    session[:error] = "Sorry, an error occurred: '#{e.message}'"
+    msg = "Sorry, an error occurred: '#{e.message}'"
   end
   
-  session[:data] = {}.merge(params)  # retain what was POSTed when redrawing the page.
-  redirect '/'
-
+  return msg
 end
